d7c8e953725f6ed86ce320c60c798c41ec018210
wakim <wakim.jraige@gmail.com>
2017-10-03 12:13:18 -0300

    Create first template file and using it in index route

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    params = {'insert_me': 'Hello i\'m from views.py!'}
+    return render(request, 'first_app/index.html', context=params)
